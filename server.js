59d8d0a2845a21ad10a64819cc53a6a396a3efe0
Sanjeev Bw <sanjeev@busywizzy.com>
2018-07-10 22:22:23 +0530

    Vid 12 comleted

--- server.js
+++ server.js
@@ -3,3 +3,21 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const request = require('request');
 const async = require('async');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res, next) => {
+	res.json("I am here");
+})
+
+app.listen(3030, (err) => {
+	if(err){
+		console.log(err);
+	} else {
+		console.log("Runnig at Port 3030");
+	}
+})
