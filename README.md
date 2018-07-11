8aab84fbc3d9618d26095a594b98aa5859d32fc5
Sanjeev Bw <sanjeev@busywizzy.com>
2018-07-11 09:32:04 +0530

    Vid 18 completed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodetwittertut
+
+
+Mailchimp
+
+sanjeevscet1@gmail.com
+
+sanjeevscetm
+
+Sanjeev!2
+
+
--- server.js
+++ server.js
@@ -15,10 +15,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
 
-
+//2d6de24f91654473d25cee696654cdd7-us18
+//https://us18.api.mailchimp.com/3.0/lists/9d488c4316/members
+///lists/{list_id}/members
+///lists/9d488c4316/members
+/*
 app.get('/', (req, res, next) => {
 	res.render('main/home.hbs');
 })
+*/
+
+app.route('/')
+	.get((req, res, next) => {
+		res.render('main/home');
+	})
+	.post((req, res, next) => {
+		request({
+			url: 'https://us18.api.mailchimp.com/3.0/lists/9d488c4316/members',
+			method: 'POST',
+			headers: {
+				'Authorization': 'randomUser 2d6de24f91654473d25cee696654cdd7-us18',
+				'Content-Type': 'application/json'
+			},
+			json: {
+				'email_address': req.body.email,
+				'status': 'subscribed'
+			}
+		}, function (err, response, body) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log('Sucessfully sent');
+				res.redirect('/');
+			}
+		})
+	})
+
+
 
 app.listen(3030, (err) => {
 	if(err){
