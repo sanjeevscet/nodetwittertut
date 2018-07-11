2b33fd7c015f46614a9e7c65a2a2476b8be89d49
Sanjeev Bw <sanjeev@busywizzy.com>
2018-07-11 07:13:30 +0530

    Vid 14 Completed

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const request = require('request');
 const async = require('async');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname:'.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
 
 
 app.get('/', (req, res, next) => {
-	res.json("I am here");
+	res.render('main/home.hbs');
 })
 
 app.listen(3030, (err) => {
